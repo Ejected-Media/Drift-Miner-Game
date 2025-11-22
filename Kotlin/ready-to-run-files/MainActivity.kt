4db88c671e957b1fdd07cc6358ba417a6e3a3f65
Kyle2501 <kyle2501@gmail.com>
2025-11-22 14:54:41 -0700

    Create MainActivity.kt

---
+++ Kotlin/ready-to-run-files/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.driftminer
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Fullscreen Immersive Mode
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        )
+        
+        setContentView(GameView(this))
+    }
+}
